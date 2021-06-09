829153827a7e6997603ccc47ed1dc72bf8e7f2bc
Johann Draschwandtner <johann.draschwandtner@stiwa.com>
2021-06-09 15:21:17 +0200

    Fixed name

--- test/test_heroapp.py
+++ test/test_heroapp.py
@@ -1,7 +1,7 @@
 def test_docker_is_installed(host):
     # testen, ob das Paket "docker-ce" installiert ist
-    docker-ce = host.package("docker-ce")
-    assert docker-ce.is_installed
+    dockerce = host.package("docker-ce")
+    assert dockerce.is_installed
 
 def test_docker_service_is_running(host):
     # testen, ob der Service "docker" läuft und verfügbar ist
